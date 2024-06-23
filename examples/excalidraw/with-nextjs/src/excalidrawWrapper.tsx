e20043fa5705a94ed65efe43419ea7e37f4799a8
HyiKi <240456539@qq.com>
2024-06-23 18:44:40 +0800

    Update excalidrawWrapper.tsx

--- examples/excalidraw/with-nextjs/src/excalidrawWrapper.tsx
+++ examples/excalidraw/with-nextjs/src/excalidrawWrapper.tsx
@@ -5,7 +5,7 @@ import "@excalidraw/excalidraw/index.css";
 
 const ExcalidrawWrapper: React.FC = () => {
   return (
-    <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+    <div style={{ width: "100vw", height: "100vh"}}>
       <Excalidraw />
     </div>
   );
