57d94d2a7e7f165847b22977864aef3052685295
HyiKi <haoxuan.wang@shopcider.com>
2024-09-27 15:57:31 +0800

    init lib

--- packages/excalidraw/components/InitializeApp.tsx
+++ packages/excalidraw/components/InitializeApp.tsx
@@ -4,6 +4,7 @@ import { LoadingMessage } from "./LoadingMessage";
 import type { Language } from "../i18n";
 import { defaultLang, languages, setLanguage } from "../i18n";
 import type { Theme } from "../element/types";
+import {API} from "../tests/helpers/api";
 
 interface Props {
   langCode: Language["code"];
@@ -19,9 +20,16 @@ export const InitializeApp = (props: Props) => {
       await setLanguage(currentLang);
       setLoading(false);
     };
+    const initLibrary = async () => {
+      // init library
+      await API.drop(
+        await API.loadFile("./library/library.excalidrawlib"),
+      );
+    }
     const currentLang =
       languages.find((lang) => lang.code === props.langCode) || defaultLang;
     updateLang();
+    initLibrary();
   }, [props.langCode]);
 
   return loading ? <LoadingMessage theme={props.theme} /> : props.children;
