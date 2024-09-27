e548c969fd76901bb1e562c30ff242b6c9b49c8e
HyiKi <haoxuan.wang@shopcider.com>
2024-09-27 17:59:10 +0800

    fix sizeHelpers.ts

--- examples/excalidraw/initialData.tsx
+++ examples/excalidraw/initialData.tsx
@@ -1,52 +1,8 @@
 import type {ExcalidrawElementSkeleton} from "@excalidraw/excalidraw/data/transform";
 import type {FileId} from "@excalidraw/excalidraw/element/types";
 
-const elements: ExcalidrawElementSkeleton[] = [
-  {
-    type: "rectangle",
-    x: 10,
-    y: 10,
-    strokeWidth: 2,
-    id: "1",
-  },
-  {
-    type: "diamond",
-    x: 120,
-    y: 20,
-    backgroundColor: "#fff3bf",
-    strokeWidth: 2,
-    label: {
-      text: "HELLO EXCALIDRAW",
-      strokeColor: "#099268",
-      fontSize: 30,
-    },
-    id: "2",
-  },
-  {
-    type: "arrow",
-    x: 100,
-    y: 200,
-    label: {text: "HELLO WORLD!!"},
-    start: {type: "rectangle"},
-    end: {type: "ellipse"},
-  },
-  {
-    type: "image",
-    x: 606.1042326312408,
-    y: 153.57729779411773,
-    width: 230,
-    height: 230,
-    fileId: "rocket" as FileId,
-  },
-  {
-    type: "frame",
-    children: ["1", "2"],
-    name: "My frame",
-  },
-];
 export default {
-  elements,
-  appState: {viewBackgroundColor: "#AFEEEE", currentItemFontFamily: 5},
+  appState: {viewBackgroundColor: "#FFFFFF", currentItemFontFamily: 5},
   scrollToContent: true,
   libraryItems: [
     [
